e5eda73665a10cf7cb3c2b8fa0304fb572559c90
thevighneshpawar <112792276+thevighneshpawar@users.noreply.github.com>
2024-07-22 13:39:55 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const swiperWrapper = document.querySelector('.swiper-wrapper');
+    const swiperSlides = document.querySelectorAll('.swiper-slide');
+    const prevButton = document.querySelector('.swiper-button-prev');
+    const nextButton = document.querySelector('.swiper-button-next');
+    const toggleele = document.querySelector('#toggleele');
+    const togglebtn = document.querySelector('#togglebtn')
+
+  
+    let currentIndex = 0;
+    let autoSlideInterval;
+  
+    function updateSwiper() {
+      const offset = -currentIndex * 100;
+      swiperWrapper.style.transform = `translateX(${offset}%)`;
+    }
+  
+    function goToNextSlide() {
+      if (currentIndex < swiperSlides.length - 1) {
+        currentIndex++;
+      } else {
+        currentIndex = 0;
+      }
+      updateSwiper();
+    }
+  
+    function goToPrevSlide() {
+      if (currentIndex > 0) {
+        currentIndex--;
+      } else {
+        currentIndex = swiperSlides.length - 1;
+      }
+      updateSwiper();
+    }
+  
+    function startAutoSlide() {
+      autoSlideInterval = setInterval(goToNextSlide, 4000); // Change slide every 3 seconds
+    }
+  
+    function stopAutoSlide() {
+      clearInterval(autoSlideInterval);
+    }
+  
+    prevButton.addEventListener('click', () => {
+      stopAutoSlide();
+      goToPrevSlide();
+      startAutoSlide();
+    });
+  
+    nextButton.addEventListener('click', () => {
+      stopAutoSlide();
+      goToNextSlide();
+      startAutoSlide();
+    });
+  
+    // Initial update
+    updateSwiper();
+    startAutoSlide();
+
+
+    togglebtn.addEventListener('click',()=>{
+      toggleele.classList.toggle('hidden');
+      toggleele.classList.toggle('visible');
+    })
+  });
+  
